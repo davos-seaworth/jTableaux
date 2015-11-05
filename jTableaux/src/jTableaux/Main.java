79562ac6a6ac26542e580c9bdc80a126e2c8a849
Andrew Braunlich <pandascide@gmail.com>
2015-11-05 17:44:39 -0600

    fixed some parsing issues in formula, still need to ensure hierarchy works and all

--- jTableaux/src/jTableaux/Main.java
+++ jTableaux/src/jTableaux/Main.java
@@ -3,6 +3,12 @@ package jTableaux;
 public class Main {
 	public static void main(String[] args)
 	{
-		
+	//	Formula f = new Formula("a'");
+	//	Formula f2 = new Formula("<>(a')");
+	//	Formula f3 = new Formula("(a)&(b)");
+		Formula f4 = new Formula("((a)&(b))|(c)");
+		System.out.println(f4.getLeftFormula().getLeftFormula().getAtom());
+		System.out.println(f4.getLeftFormula().getRightFormula().getAtom());
+		System.out.println(f4.getRightFormula().getAtom());
 	}
 }
