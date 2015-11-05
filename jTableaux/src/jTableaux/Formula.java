2956d60b749d83383faffb1c0cd116e16bf8f135
Andrew Braunlich <pandascide@gmail.com>
2015-11-05 14:36:00 -0600

    Update Formula.java
    more function definitions

--- jTableaux/src/jTableaux/Formula.java
+++ jTableaux/src/jTableaux/Formula.java
@@ -7,9 +7,39 @@ public class Formula {
 		
 	}
 	
+	public Formula(Formula a, Formula b, String conn)
+	{
+		connective = conn;
+		subformulas = {a, b};
+	}
+	
+	public Formula(Formula a, String conn)
+	{
+		connective = conn;
+		subformulas = {a};
+	}
+	
 	public String getMainConnective()
 	{
 		return connective;
+	}
+	
+	public Formula getLeftFormula()
+	{
+		return subformulas[0];
+	}
+	
+	public Formula getRightFormula()
+	{
+		if isAtomic()
+			return NULL;
+		return subformulas[1];
+	}
+	
+	
+	public boolean isAtomic()
+	{
+		return connective.equals("");
 	}
 	
 	public boolean hasBeenused()
