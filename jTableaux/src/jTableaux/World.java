d7e6db5bd2091760931e827e0baf8fe585eaf626
Andrew Braunlich <pandascide@gmail.com>
2015-11-05 10:36:40 -0600

    Added Formula and World classes with some basic necessary function definitions and variables

---
+++ jTableaux/src/jTableaux/World.java
@@ -0,0 +1,24 @@
+package jTableaux;
+
+import java.util.ArrayList;
+
+public class World {
+	
+	public World()
+	{
+		
+	}
+	
+	public World(Formula f)
+	{
+		
+	}
+	
+	public void add(Formula f)
+	{
+		
+	}
+	
+	private ArrayList<World> related_worlds = new ArrayList<World>();
+	
+}
