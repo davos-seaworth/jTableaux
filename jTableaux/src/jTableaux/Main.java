41da7e3605d1a9cecf1fbea0918185e815867585
Andrew Braunlich <pandascide@gmail.com>
2015-11-05 18:36:32 -0600

    Formulas are all set up- I think!, though the double formula input constructor doesn't do anything. Perhaps for terminal input- maybe.

--- jTableaux/src/jTableaux/Main.java
+++ jTableaux/src/jTableaux/Main.java
@@ -6,9 +6,18 @@ public class Main {
 	//	Formula f = new Formula("a'");
 	//	Formula f2 = new Formula("<>(a')");
 	//	Formula f3 = new Formula("(a)&(b)");
-		Formula f4 = new Formula("((a)&(b))|(c)");
+		
+		/**Formula f4 = new Formula("((a)&(b))|(c)");
 		System.out.println(f4.getLeftFormula().getLeftFormula().getAtom());
 		System.out.println(f4.getLeftFormula().getRightFormula().getAtom());
-		System.out.println(f4.getRightFormula().getAtom());
+		System.out.println(f4.getRightFormula().getAtom());**/
+		
+		Formula p = new Formula("((a)->(d))&((b)|(c))");
+		Formula c = new Formula("b");
+		//p.printForm();
+		World w = new World();
+		w.addPremise(p);
+		w.addConclusion(c);
+		w.printFormulas();
 	}
 }
