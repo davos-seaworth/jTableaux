c1368aa44dd3f322f5a21f3bcb91c824e951719c
Andrew Braunlich <pandascide@gmail.com>
2015-11-07 10:56:46 -0600

    branches added and i think they work
    todo: !& and figuring out why it duplicates some worlds

--- jTableaux/src/jTableaux/World2.java
+++ jTableaux/src/jTableaux/World2.java
@@ -16,10 +16,11 @@ public class World2 {
 		{
 			Formula f = formulas.get(i);
 			//System.out.println(f.getMainConnective()+"========");
+			if(!f.hasBeenused()){
 			switch(f.getMainConnective())
 			{
 			
-			case "!!":
+			case "!!":f.toggleUsed();
 				if(f.getLeftFormula().getLeftFormula().isSingleConnective()||f.getLeftFormula().getLeftFormula().isAtomic())
 				{
 					Formula g = new Formula(f.getLeftFormula().getLeftFormula());
@@ -33,7 +34,7 @@ public class World2 {
 						formulas.add(g);
 				}
 				break;
-			case "!|":
+			case "!|":f.toggleUsed();
 				Formula g = new Formula(f.getLeftFormula().getLeftFormula(),"!");
 				Formula h = new Formula(f.getLeftFormula().getRightFormula(),"!");
 				if(!alreadyHere(g))
@@ -41,7 +42,7 @@ public class World2 {
 				if(!alreadyHere(h))
 					formulas.add(h);
 				break;
-			case "&":
+			case "&":f.toggleUsed();
 				Formula g1 = new Formula(f.getLeftFormula());
 				Formula h1 = f.getRightFormula();		//why does this work?
 				if(!alreadyHere(g1))
@@ -49,7 +50,7 @@ public class World2 {
 				if(!alreadyHere(h1))
 					formulas.add(h1);
 				break;
-			case "!->":
+			case "!->":f.toggleUsed();
 				Formula g2 = new Formula(f.getLeftFormula().getLeftFormula());
 				Formula h2 = new Formula(f.getLeftFormula().getRightFormula(),"!");
 				if(!alreadyHere(g2))
@@ -57,12 +58,12 @@ public class World2 {
 				if(!alreadyHere(h2))
 					formulas.add(h2);
 				break;
-			case "![]":
+			case "![]":f.toggleUsed();
 				World2 v = new World2(name+"R"+name+"_v | created by: " + f.renderAsString());
 				v.addFormula(new Formula(f.getLeftFormula().getLeftFormula(),"!"));
 				related_worlds.add(v);
 				break;
-			case "[]":
+			case "[]":f.toggleUsed();
 				for(int k=0;k<related_worlds.size();k++)
 				{//related_worlds.get(k).addFormula(
 					Formula t = new Formula(f.getLeftFormula());
@@ -70,12 +71,12 @@ public class World2 {
 						related_worlds.get(k).addFormula(t);
 				}
 				break;
-			case "<>":
+			case "<>":f.toggleUsed();
 				World2 v2 = new World2(name+"R"+name+"_v | created by: " + f.renderAsString());
 				v2.addFormula(new Formula(f.getLeftFormula()));
 				related_worlds.add(v2);
 				break;
-			case "!<>":
+			case "!<>":f.toggleUsed();
 				for(int k=0;k<related_worlds.size();k++)
 				{//related_worlds.get(k).addFormula(
 					Formula t = new Formula(f.getLeftFormula().getLeftFormula(),"!");
@@ -83,22 +84,74 @@ public class World2 {
 						related_worlds.get(k).addFormula(t);
 				}
 				break;
-			case "|":
+			case "|":f.toggleUsed();
 				Formula g3 = new Formula(f.getLeftFormula());
-				
-				
+				World2 v1 = new World2(this);
+				formulas.add(g3);
+				v1.addFormula(f.getRightFormula());
+				branches.add(v1);
+				break;
+			case "->":f.toggleUsed();
+				Formula g4 = new Formula(f.getLeftFormula());
+				World2 v3 = new World2(this);
+				formulas.add(new Formula(g4,"!"));
+				v3.addFormula(f.getRightFormula());
+				branches.add(v3);
+				break;
 			}
+			}
+
 		}
 		for(int i=0;i<related_worlds.size();i++)
 		{
 			related_worlds.get(i).doRules();
 		}
+		for(int i=0;i<branches.size();i++)
+			branches.get(i).doRules();
 	}
 	
 	
 	public World2(World2 w)
 	{
 		name = w.getName();
+		ArrayList<Formula> newform = w.getFormulas();
+		ArrayList<World2> newrelated = w.getRelated();
+		ArrayList<World2> newbrnch = w.getBranches();
+		for (int i=0;i<newform.size();i++)
+		{
+			
+			formulas.add(new Formula(newform.get(i)));
+			if(newform.get(i).hasBeenused())
+				formulas.get(i).toggleUsed();
+		}
+		for (int i=0;i<newrelated.size();i++)
+		{
+			related_worlds.add(new World2(newrelated.get(i)));
+		}
+		for (int i=0;i<newrelated.size();i++)
+		{
+			related_worlds.add(new World2(newrelated.get(i)));
+		}
+		for (int i=0;i<newbrnch.size();i++)
+		{
+			related_worlds.add(new World2(newbrnch.get(i)));
+		}		
+		
+	}
+	
+	public ArrayList<World2> getBranches()
+	{
+		return branches;
+	}
+	
+ 	public ArrayList<World2> getRelated()
+	{
+		return related_worlds;
+	}
+	
+	public ArrayList<Formula> getFormulas()
+	{
+		return formulas;
 	}
 	
 	public String getName()
@@ -141,6 +194,11 @@ public class World2 {
 			System.out.println("**This world is related to: "+name+"**");
 			related_worlds.get(i).printBranch();
 		}
+		for(int i=0;i<branches.size();i++)
+		{
+			System.out.println("**This world branches from: "+name);
+			branches.get(i).printBranch();
+		}
 	}
 	
 	public void addPremise(Formula f)
@@ -159,7 +217,7 @@ public class World2 {
 	}
 	
 	
-	public ArrayList<World> branches = new ArrayList<World>();
+	public ArrayList<World2> branches = new ArrayList<World2>();
 	
 	private String name;
 	public ArrayList<Formula> formulas = new ArrayList<Formula>();
